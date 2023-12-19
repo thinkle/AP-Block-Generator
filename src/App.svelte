ad5c2ccb8fffa6e289c4e3626b564f4ea3bcee4c
Tom Hinkle <tmhinkle@gmail.com>
2023-12-18 20:14:40 -0500

    ++run button

--- src/App.svelte
+++ src/App.svelte
@@ -8,27 +8,50 @@
   import { parseCode } from "./lib/parseCode";
   import PseudoCode from "./components/APCSP/PseudoCode.svelte";
   import type { AnyElement } from "./lib/pseudocode";
+  import CodeResult from "./components/CodeResult.svelte";
 
   let parsed: AnyElement | AnyElement[] = [];
   $: parsed = parseCode($code);
+  let showResult = false;
 </script>
 
 <h1>TypeScript => APCSP PseudoCode Generator</h1>
+
 <main>
   <ThreeColumn>
-    <div slot="left"><CodeMirror bind:value={$code} lang={javascript()} /></div>
-    <div slot="center"><PseudoCode node={parsed} /></div>
+    <div slot="left">
+      <div class="bar">
+        <h2>JavaScript</h2>
+        {#if showResult}
+          <button on:click={() => (showResult = false)}>Reset</button>
+        {:else}
+          <button on:click={() => (showResult = true)}>Run</button>
+        {/if}
+      </div>
+      <CodeMirror bind:value={$code} lang={javascript()} />
+    </div>
+    <div slot="center">
+      <div class="bar"><h2>APCSP Blocks</h2></div>
+      <PseudoCode node={parsed} />
+    </div>
     <div slot="right">
+      <div class="bar"><h2>APCSP PseudoCode</h2></div>
       <PseudoText node={parsed}></PseudoText>
-      <!--   <PseudoCodeText node={parseCode($code)} /> -->
     </div>
   </ThreeColumn>
 </main>
-
-<h2>Debug: JSON Parse Tree Belwo:</h2>
-<pre>
-  {JSON.stringify(parseCode($code), null, 2)}
-</pre>
+<section>
+  {#if showResult}
+    <section class="result">
+      <button on:click={() => (showResult = false)}>Hide</button>
+      <CodeResult
+        height={200}
+        js={$code}
+        onWindowLoaded={() => console.log("Loaded code")}
+      />
+    </section>
+  {/if}
+</section>
 
 <style>
   h1 {
@@ -57,5 +80,29 @@
   }
   .read-the-docs {
     color: #888;
+  }
+  .bar {
+    height: 38px;
+    background-color: #888;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .bar h2 {
+    margin: 0;
+  }
+  .bar button {
+    position: absolute;
+    right: 8px;
+    top: 8px;
+    height: 22px;
+    border: none;
+    background-color: #646cff;
+    color: white;
+    border-radius: 4px;
+    padding: 0 8px;
+    cursor: pointer;
   }
 </style>
