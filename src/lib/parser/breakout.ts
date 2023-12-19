ee98a18af0c916a310c7fca5504c4de6835b532a
Tom Hinkle <tmhinkle@gmail.com>
2023-12-19 14:20:22 -0500

    Quiet down

--- src/lib/parser/breakout.ts
+++ src/lib/parser/breakout.ts
@@ -12,7 +12,6 @@ export function transformTreeWithBreakout(
   node: AnyElement,
   appendSiblings?: (nodes: AnyElement[]) => void
 ): void {
-  console.log("transformTree from ", node, appendSiblings);
   // We can only act on great-grandchildren...
   let children: AnyElement[] = [];
 
@@ -32,11 +31,15 @@ export function transformTreeWithBreakout(
     ].includes(node.element)
   ) {
     /* Items with a "body" block whose children are our children */
-    checkChildren(node.body.children);
+    if (node.body && node.body.children) {
+      checkChildren(node.body.children);
+    }
   } else if (node.element == "ifStatement") {
     /* "If" statements have *two* sets of children */
     checkChildren(node.consequent.children);
-    checkChildren(node.alternate.children);
+    if (node.alternate) {
+      checkChildren(node.alternate.children);
+    }
   } else if (node.element == "mathExpression") {
     /* We expect we may have inserted an array into the "right" branch */
     if (Array.isArray(node.right)) {
@@ -51,18 +54,13 @@ export function transformTreeWithBreakout(
     }
   }
   function checkChildren(children: (AnyElement | { element: "breakOut" })[]) {
+    if (!children) {
+      return;
+    }
     for (let i = 0; i < children.length; i++) {
       // We define the method we would use to "break out" any grandchildren...
       // when we call ourselves recursively...
       const appendGrandSiblings = (nodes: AnyElement[]) => {
-        console.log(
-          "Calling appendGrandSiblings with",
-          nodes,
-          " moving into ",
-          children,
-          "at ",
-          i
-        );
         children.splice(i + 1, 0, ...nodes);
       };
       let child = children[i];
@@ -72,11 +70,8 @@ export function transformTreeWithBreakout(
         // remove the node we just added, so we need to skip it
         if (appendSiblings) {
           let deletedNode = children.splice(i, 1);
-          console.log("Deleted node", deletedNode, "from", children);
           let sibsToMove = children.splice(i, children.length - i);
-          console.log("Sibs to move", sibsToMove);
           appendSiblings(sibsToMove);
-          console.log("Moved sibs up...");
         } else {
           child.element = "value";
           child.value = "BREAKOUT NODE AT TOP LEVEL???";
