8e0708f40beadc976fb3bc2b39b31a94d93dfb55
Tom Hinkle <tmhinkle@gmail.com>
2023-12-20 11:21:47 -0500

    Move utility function

--- src/lib/parser/handleWhileLoop.ts
+++ src/lib/parser/handleWhileLoop.ts
@@ -2,7 +2,7 @@ import * as TS from "ts-morph";
 import type { UntilLoopExpression } from "../pseudocode";
 import type { Block } from "../pseudocode";
 import { processNode } from "."; // Assuming you have a function to invert conditions
-import { getStatementFromJS } from "./utils/getStatementFromJS";
+import { invertCondition } from "./utils/invertCondition";
 
 export const handleWhileLoop = (
   node: TS.WhileStatement
@@ -30,27 +30,3 @@ export const handleWhileLoop = (
     body,
   };
 };
-
-function invertCondition(expression: TS.Expression): TS.Expression {
-  // Direct negation for simple conditions
-  if (
-    expression.getKind() === TS.SyntaxKind.PrefixUnaryExpression &&
-    expression.getOperatorToken() === TS.SyntaxKind.ExclamationToken
-  ) {
-    // Remove the negation if it's already negated (e.g., !condition becomes condition)
-    return expression.getOperand();
-  }
-
-  // Apply De Morgan's laws for compound conditions (e.g., !(a && b) becomes !a || !b)
-  // This is a simplified example and should be expanded to handle various cases
-
-  // For other cases, simply negate the condition
-  return getStatementFromJS(`!${expression.getText()}`);
-  /* const project = new TS.Project({ useInMemoryFileSystem: true });
-  const sourceFile = project.createSourceFile(
-    "temp.ts",
-    `!${expression.getText()}`,
-    { overwrite: true }
-  );
-  return sourceFile.getStatements()[0] as TS.Expression; */
-}
