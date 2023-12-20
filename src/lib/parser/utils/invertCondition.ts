49fab839f7b707ba235e54b30f14cbb7380efd53
Tom Hinkle <tmhinkle@gmail.com>
2023-12-20 12:27:55 -0500

    Do some simple handling of basic == != > < statements

--- src/lib/parser/utils/invertCondition.ts
+++ src/lib/parser/utils/invertCondition.ts
@@ -10,16 +10,52 @@ export function invertCondition(expression: TS.Expression): TS.Expression {
     // Remove the negation if it's already negated (e.g., !condition becomes condition)
     return expression.getOperand();
   }
+  /* Can we check for simple statements, such as... 
+  == -> !=
+  != -> ==
+  < -> >=
+  > -> <=
+  */
+  // Check for simple binary expressions
+  if (expression.getKind() === TS.SyntaxKind.BinaryExpression) {
+    debugger;
+    const binaryExpr = expression as TS.BinaryExpression;
+    const left = binaryExpr.getLeft();
+    const right = binaryExpr.getRight();
+    let newOperator: TS.SyntaxKind | null = null;
+    let leftText = left.getText();
+    let rightText = right.getText();
+    let operatorText = binaryExpr.getOperatorToken().getText();
+    operatorText = operatorText.replace(/([=!])=/, "$1==");
+    switch (operatorText) {
+      case "===":
+        newOperator = "!==";
+        break;
+      case "!==":
+        newOperator = "==";
+        break;
+      case "<":
+        newOperator = ">=";
+        break;
+      case ">":
+        newOperator = "<=";
+        break;
+      case "<=":
+        newOperator = ">";
+        break;
+      case ">=":
+        newOperator = "<";
+        break;
+      // Add more cases as needed
+    }
 
-  // Apply De Morgan's laws for compound conditions (e.g., !(a && b) becomes !a || !b)
-  // This is a simplified example and should be expanded to handle various cases
-  // For other cases, simply negate the condition
-  return getStatementFromJS(`!${expression.getText()}`);
-  /* const project = new TS.Project({ useInMemoryFileSystem: true });
-  const sourceFile = project.createSourceFile(
-    "temp.ts",
-    `!${expression.getText()}`,
-    { overwrite: true }
-  );
-  return sourceFile.getStatements()[0] as TS.Expression; */
+    if (newOperator) {
+      return getStatementFromJS(
+        `${left.getText()} ${newOperator} ${right.getText()}`
+      );
+    }
+  }
+
+  let jsExpression = `!(${expression.getText()})`;
+  return getStatementFromJS(jsExpression);
 }
