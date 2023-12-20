0295ad015af4e3ab88f317ed44cb514e670760c3
Tom Hinkle <tmhinkle@gmail.com>
2023-12-19 22:00:04 -0500

    Allow moving before as well as after. Check more nodes.

--- src/lib/parser/breakout.ts
+++ src/lib/parser/breakout.ts
@@ -14,7 +14,9 @@ export function transformTreeWithBreakout(
 ): void {
   // We can only act on great-grandchildren...
   let children: AnyElement[] = [];
-
+  if (node.value && Array.isArray(node.value)) {
+    checkChildren(node.value);
+  }
   /* We have numerous types of "children" because yours truly was
   too bone-headed to stick with a simple node=>children design...  */
   if (node.element === "code" || node.element === "block") {
@@ -36,6 +38,7 @@ export function transformTreeWithBreakout(
     }
   } else if (node.element == "ifStatement") {
     /* "If" statements have *two* sets of children */
+    checkChildren(node.condition);
     checkChildren(node.consequent.children);
     if (node.alternate) {
       checkChildren(node.alternate.children);
@@ -52,6 +55,11 @@ export function transformTreeWithBreakout(
         checkChildren(arg);
       }
     }
+  } else if (node.element == "returnStatement") {
+    /* We expect we may have inserted an array into the "value" */
+    if (Array.isArray(node.value)) {
+      checkChildren(node.value);
+    }
   }
   function checkChildren(children: (AnyElement | { element: "breakOut" })[]) {
     if (!children) {
@@ -60,8 +68,15 @@ export function transformTreeWithBreakout(
     for (let i = 0; i < children.length; i++) {
       // We define the method we would use to "break out" any grandchildren...
       // when we call ourselves recursively...
-      const appendGrandSiblings = (nodes: AnyElement[]) => {
-        children.splice(i + 1, 0, ...nodes);
+      const appendGrandSiblings = (
+        nodes: AnyElement[],
+        direction: "before" | "after" = "after"
+      ) => {
+        if (direction === "after") {
+          children.splice(i + 1, 0, ...nodes);
+        } else {
+          children.splice(i, 0, ...nodes);
+        }
       };
       let child = children[i];
       // ...and then we call it on each child
@@ -69,9 +84,10 @@ export function transformTreeWithBreakout(
         // We need to remove the breakout node, but we don't want to
         // remove the node we just added, so we need to skip it
         if (appendSiblings) {
+          let direction = child.direction || "after";
           let deletedNode = children.splice(i, 1);
           let sibsToMove = children.splice(i, children.length - i);
-          appendSiblings(sibsToMove);
+          appendSiblings(sibsToMove, direction);
         } else {
           child.element = "value";
           child.value = "BREAKOUT NODE AT TOP LEVEL???";
