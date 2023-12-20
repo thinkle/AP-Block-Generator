dc083854d049dd02ccf9ce6168ed2d37565ef6f0
Tom Hinkle <tmhinkle@gmail.com>
2023-12-20 10:43:56 -0500

    ++until loop

---
+++ src/components/APCSP/UntilLoop.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import * as P from "../../lib/pseudocode";
+  import Block from "./Block.svelte";
+  import PseudoCode from "./PseudoCode.svelte";
+  export let node: P.UntilLoopExpression;
+  console.log("Got node", node);
+</script>
+
+<div class="shaded round-block">
+  <div class="line">
+    <span class="keyword">REPEAT UNTIL</span>
+    <PseudoCode node={node.condition} />
+  </div>
+  <div class="rounded-block">
+    <PseudoCode node={node.body} />
+  </div>
+</div>
